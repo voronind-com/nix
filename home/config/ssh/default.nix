fdff73c645fa7e111641e8095f673b163dde42df
Dmitry Voronin <hi@voronind.com>
2024-08-24 17:41:18 +0300

    Ssh : Persist for all connections.

--- home/config/ssh/default.nix
+++ home/config/ssh/default.nix
@@ -1,25 +1,27 @@
 # SSH client configuration.
 { util, ... }: let
-	mkHost = name: address: port: user: extra: util.trimTabs ''
+	mkHost = name: address: port: user: extra: ''
 		Host ${name}
 			HostName ${address}
 			User ${user}
 			Port ${toString port}
-			ControlPath ~/.ssh/${name}.socket
-			ControlMaster auto
-			ControlPersist yes
 	'' + extra;
 in {
-	text = ""
-		+ mkHost "dasha"    "10.0.0.7"       22143 "root" ""
-		+ mkHost "desktop"  "10.0.0.3"       22143 "root" ""
-		+ mkHost "fmpmaven" "10.30.22.10"    22    "root" ""
-		+ mkHost "home"     "10.0.0.1"       22143 "root" ""
-		+ mkHost "laptop"   "192.168.1.9"    22143 "root" ""
-		+ mkHost "pi"       "192.168.1.6"    22143 "root" ""
-		+ mkHost "vpn"      "194.113.233.38" 22143 "root" ""
-		+ mkHost "work"     "192.168.1.5"    22143 "root" ""
+	text = util.trimTabs (''
+		Host *
+		ControlMaster auto
+		ControlPath ~/.ssh/%r@%h:%p
+		ControlPersist yes
+	''
+	+ mkHost "dasha"    "10.0.0.7"       22143 "root" ""
+	+ mkHost "desktop"  "10.0.0.3"       22143 "root" ""
+	+ mkHost "fmpmaven" "10.30.22.10"    22    "root" ""
+	+ mkHost "home"     "10.0.0.1"       22143 "root" ""
+	+ mkHost "laptop"   "192.168.1.9"    22143 "root" ""
+	+ mkHost "pi"       "192.168.1.6"    22143 "root" ""
+	+ mkHost "vpn"      "194.113.233.38" 22143 "root" ""
+	+ mkHost "work"     "192.168.1.5"    22143 "root" ""
 
-		+ mkHost "nixbuilder" "10.0.0.1"    "22143" "nixbuilder" "	StrictHostKeyChecking=accept-new"
-		;
+	+ mkHost "nixbuilder" "10.0.0.1" 22143 "nixbuilder" "	StrictHostKeyChecking=accept-new"
+	);
 }
