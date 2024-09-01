a2d6a480e4a49f448071ccdd2babb2d377f2e8d2
Dmitry Voronin <hi@voronind.com>
2024-09-01 04:25:00 +0300

    Tmux : Remap session switch to <>.

--- home/config/tmux/module/Session.nix
+++ home/config/tmux/module/Session.nix
@@ -3,8 +3,8 @@
 		bind -n M-z detach-client
 		bind -n M-Z detach-client -a
 
-		bind -n M-( switch-client -p
-		bind -n M-) switch-client -n
+		bind -n M-, switch-client -p
+		bind -n M-. switch-client -n
 		bind -n M-g choose-session -Z
 
 		set -g window-size smallest
