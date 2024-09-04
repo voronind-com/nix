eb750fb356bf95fc6e2aa2ae8fd8f3db45debfb8
Dmitry Voronin <hi@voronind.com>
2024-09-04 04:14:52 +0300

    Yazi : Add D to delete without confirmation.

--- home/config/yazi/module/Keymap.nix
+++ home/config/yazi/module/Keymap.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }: {
 	file = (pkgs.formats.toml {}).generate "YaziKeymapConfig" {
 		manager = {
-			prepend_keymap = [{
-				on   = "d";
-				run  = "remove --permanently";
-				desc = "Dangerous life.";
-			}];
+			prepend_keymap = [
+				{
+					on   = "d";
+					run  = "remove --permanently";
+					desc = "Dangerous life.";
+				}
+				{
+					on   = "D";
+					run  = "remove --permanently --force";
+					desc = "Dangerous life.";
+				}
+			];
 		};
 	};
 }
