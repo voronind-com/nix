bce9ad204ddfc885a2999c94217b34e2002d6e39
Dmitry Voronin <hi@voronind.com>
2024-09-07 22:33:28 +0300

    Display : Use max fps by default and limit per-game.

--- package/swayscript/script/Gaming.nix
+++ package/swayscript/script/Gaming.nix
@@ -4,9 +4,9 @@
 		function gamingon() {
 			on() {
 				swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" adaptive_sync on"
-				swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" mode 3440x1440@164.999Hz"
+				# swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" mode 3440x1440@164.999Hz"
 				swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" adaptive_sync on"
-				swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" mode 1920x1080@144.000Hz"
+				# swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" mode 1920x1080@144.000Hz"
 				_gamingstate on
 			}
 			_sway_iterate_sockets on
@@ -16,9 +16,9 @@
 		function gamingoff() {
 			off() {
 				swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" adaptive_sync off"
-				swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" mode 3440x1440@59.973Hz"
+				# swaymsg "output \"Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622\" mode 3440x1440@59.973Hz"
 				swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" adaptive_sync off"
-				swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" mode 1920x1080@60.000Hz"
+				# swaymsg "output \"AOC 24G2W1G4 ATNL61A129625\" mode 1920x1080@60.000Hz"
 				_gamingstate off
 			}
 			_sway_iterate_sockets off
