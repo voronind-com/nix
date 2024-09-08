eb29b43033060111bb8e359c4c45b20fc5b48f72
Dmitry Voronin <hi@voronind.com>
2024-09-08 18:42:31 +0300

    Vm : Add some extra params.

--- module/Vm.nix
+++ module/Vm.nix
@@ -1,4 +1,5 @@
 { lib, ... }: {
+	# SEE: https://github.com/name-snrl/nixos-configuration/blob/e7f6b0f664dbee82e3bf3e85a98cdc3088abe602/modules/nixos/profiles/system/vm-config.nix#L1
 	virtualisation.vmVariant = {
 		module = {
 			autoupdate.enable     = lib.mkForce false;
@@ -6,13 +7,19 @@
 			keyd.enable           = lib.mkForce false;
 		};
 		virtualisation = {
-			memorySize = 4  * 1024;
-			diskSize   = 20 * 1024;
+			# diskSize   = 20 * 1024;
 			cores      = 4;
+			diskImage  = null;
+			memorySize = 4  * 1024;
+			msize      = 1024 * 1024;
 			restrictNetwork = false;
 			resolution = {
 				x = 1280;
 				y = 720;
+			};
+			sharedDirectories.experiments = {
+				source = "$HOME";
+				target = "/mnt/home";
 			};
 		};
 	};
