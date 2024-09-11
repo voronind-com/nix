64537ac3e23a79337e089c76ea3ceeaf31468562
Dmitry Voronin <hi@voronind.com>
2024-09-11 18:40:30 +0300

    Sway : Move gaming & montoggle to sway mod.

--- home/config/sway/module/Display.nix
+++ home/config/sway/module/Display.nix
@@ -8,5 +8,9 @@
 		output * scale 1
 		output "Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622" pos 0,1080
 		output "ASUSTek COMPUTER INC ASUS VA24E R2LMTF127165" mode 1920x1080@74.986Hz transform 180 pos 780,0
+
+		# Display toggles.
+		bindsym --to-code $mod+m exec 'swayscript montoggle'
+		bindsym --to-code $mod+g exec 'swayscript gamingtoggle'
 	'';
 }
--- home/config/sway/module/System.nix
+++ home/config/sway/module/System.nix
@@ -4,9 +4,7 @@ in {
 	text = ''
 		bindsym ${mod} input * xkb_switch_layout 0
 		bindsym --to-code ${mod}+c exec 'systemctl reboot -i'
-		bindsym --to-code ${mod}+g exec 'swayscript gamingtoggle'
 		bindsym --to-code ${mod}+l exec 'powerlimit toggle'
-		bindsym --to-code ${mod}+m exec 'swayscript montoggle'
 		bindsym --to-code ${mod}+p exec 'powersave toggle'
 		bindsym --to-code ${mod}+r exec 'swayscript reload'
 		bindsym --to-code ${mod}+v exec 'swayscript vpntoggle'
