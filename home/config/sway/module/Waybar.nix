e6edf477f71d7cd2a522001e3f0e23dfab2b9760
Dmitry Voronin <hi@voronind.com>
2024-09-16 06:45:49 +0300

    Sway : Hard kill waybar instead of a SIGUSR1 toggle.

--- home/config/sway/module/Waybar.nix
+++ home/config/sway/module/Waybar.nix
@@ -1,6 +1,6 @@
 { ... }: {
 	text = ''
-		bindsym --to-code $mod+shift+r exec 'pkill -SIGUSR1 waybar || waybar'
+		bindsym --to-code $mod+shift+r exec 'pkill waybar || exec waybar'
 		exec waybar
 		exec nm-applet
 		exec blueman-applet
