e7a4d10af28f9d18babc9949ccda08c0ec91d0c4
Dmitry Voronin <hi@voronind.com>
2024-10-04 03:57:43 +0300

    Sway : Move desktop-specific display to host config.

--- host/x86_64-linux/desktop/default.nix
+++ host/x86_64-linux/desktop/default.nix
@@ -22,6 +22,8 @@
 		desktop.sway = {
 			enable = true;
 			extraConfig = ''
+				output "ASUSTek COMPUTER INC ASUS VA24E R2LMTF127165" mode 1920x1080@74.986Hz transform 180 pos 780,0
+				output "Huawei Technologies Co., Inc. ZQE-CBA 0xC080F622" pos 0,1080
 				workspace 1 output "ASUSTek COMPUTER INC ASUS VA24E R2LMTF127165"
 			'';
 		};
