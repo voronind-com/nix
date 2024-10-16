58f3680d3c352446dd632cc5da01e315972eddb9
Dmitry Voronin <hi@voronind.com>
2024-10-16 16:18:03 +0300

    Neovim: Also silence errors.

--- home/config/nvim/module/config/Notify.nix
+++ home/config/nvim/module/config/Notify.nix
@@ -5,12 +5,15 @@
 
     -- Disable error messages popup.
     -- Instead print them and write to /tmp/NeovimError<PID>.txt
-    vim.notify = function(msg, log_level, opts)
-      print(string.sub(msg, 1, vim.v.echospace))
+    bequiet = function(msg, log_level, opts)
+      print(string.sub(tostring(msg), 1, vim.v.echospace))
 
       local file = io.open("/tmp/NeovimError"..tostring(pid)..".txt", "a")
       file:write(msg.."\n")
       file:close()
     end
+
+    vim.notify = bequiet
+    vim.api.nvim_err_writeln = bequiet
   '';
 }
