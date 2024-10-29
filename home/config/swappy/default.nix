f57cf3bbf20a4bb40034e8592cc88273bc253d6a
Dmitry Voronin <hi@voronind.com>
2024-10-29 03:44:27 +0300

    Swappy: Add custom_color ahead of time.

--- home/config/swappy/default.nix
+++ home/config/swappy/default.nix
@@ -2,6 +2,7 @@
 {
   config = (pkgs.formats.ini { }).generate "SwappyConfig" {
     Default = {
+      custom_color = with config.style.color; "rgba(${accent-r},${accent-g},${accent-b},1)";
       early_exit = true;
       fill_shape = false;
       line_size = 4;
