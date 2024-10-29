c26fdcae81888f7c62f8fe749618f54e87b2faa0
Dmitry Voronin <hi@voronind.com>
2024-10-29 11:44:43 +0300

    Wallpaper: Use contrast text.

--- config/Wallpaper.nix
+++ config/Wallpaper.nix
@@ -3,7 +3,7 @@ with lib;
 let
   url = "https://i.imgur.com/3fJXekt.jpeg";
   sha256 = "sha256-3ZyFZwWuVg4oXZ0zfN2LuPgf7KEFdTS3zWd1FYScOdQ=";
-  forceContrastText = false;
+  forceContrastText = true;
 in
 {
   options = {
