524efdf533420b4ec938f85b4075eee9d6526a7b
Dmitry Voronin <hi@voronind.com>
2024-11-17 23:35:48 +0300

    Sway: Fix border changes.

--- home/file/sway/module/Style.nix
+++ home/file/sway/module/Style.nix
@@ -4,16 +4,15 @@
 }: let
 	accent = config.module.style.color.accent  + alpha;
 	alpha  = config.module.style.opacity.hex;
-	bg     = config.module.style.color.bg.dark + alpha;
 	border = config.module.style.color.border  + alpha;
 	fg     = config.module.style.color.fg.light;
 in {
 	text = ''
 		output * bg ${config.module.wallpaper.path} fill
 		client.focused          "#${accent}" "#${accent}" "#${fg}" "#${accent}" "#${accent}"
-		client.focused_inactive "#${border}" "#${bg}"     "#${fg}" "#${border}" "#${border}"
-		client.unfocused        "#${border}" "#${bg}"     "#${fg}" "#${border}" "#${border}"
-		client.urgent           "#${border}" "#${bg}"     "#${fg}" "#${border}" "#${border}"
-		client.placeholder      "#${bg}"     "#${bg}"     "#${fg}" "#${bg}"     "#${bg}"
+		client.focused_inactive "#${border}" "#${border}" "#${fg}" "#${border}" "#${border}"
+		client.unfocused        "#${border}" "#${border}" "#${fg}" "#${border}" "#${border}"
+		client.urgent           "#${border}" "#${border}" "#${fg}" "#${border}" "#${border}"
+		client.placeholder      "#${border}" "#${border}" "#${fg}" "#${border}" "#${border}"
 	'';
 }
