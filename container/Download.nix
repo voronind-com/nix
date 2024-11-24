1c43a448ef474782bc046691ef5efb1390ccb0b5
Dmitry Voronin <hi@voronind.com>
2024-11-25 00:29:13 +0300

    Download: Restart every 3h.

--- container/Download.nix
+++ container/Download.nix
@@ -55,7 +55,7 @@ in {
 					systemd.services.deluged.serviceConfig = {
 						MemoryLimit   = cfg.memLimit;
 						Restart       = lib.mkForce "always";
-						RuntimeMaxSec = "6h";
+						RuntimeMaxSec = "3h";
 					};
 				};
 		};
