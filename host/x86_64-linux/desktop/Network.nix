0a1dfb0b841a833ea53b179ad64d2a2a5d7108f6
Dmitry Voronin <hi@voronind.com>
2024-11-28 22:06:48 +0300

    Desktop: Add network config.

---
+++ host/x86_64-linux/desktop/Network.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+	systemd.network = {
+		networks = {
+			"10-lan" = {
+				matchConfig.Name = "enp4s0";
+				linkConfig.RequiredForOnline = "routable";
+				networkConfig = {
+					DHCP = "ipv4";
+					IPv6AcceptRA = true;
+				};
+				address = [
+					"10.0.0.3/24"
+				];
+				routes = [
+					{ routeConfig.Gateway = "10.0.0.1"; }
+				];
+			};
+		};
+	};
+}
