93cfe699e476c0b5db1b113c037090b599e5be52
Dmitry Voronin <hi@voronind.com>
2024-11-28 22:28:13 +0300

    Network: Add manual comment.

--- system/Network.nix
+++ system/Network.nix
@@ -1,11 +1,9 @@
-{
-	lib,
-	...
-}: {
+{ ... }: {
 	# REF: https://nixos.wiki/wiki/Systemd-networkd
+	# SEE: man 5 systemd.network
 	systemd.network = {
 		enable = true;
-		wait-online.enable = false; # So we can use both NM and networkd.
+		wait-online.enable = false; # HACK: So we can use both NM and networkd.
 	};
 
 	networking = {
