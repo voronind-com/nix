afd3d5d80ef1e2a7142717565fbcd8bd0e8bccf8
Dmitry Voronin <hi@voronind.com>
2024-12-02 01:29:11 +0300

    Nix: Allow insecure dotnet.

--- system/Nix.nix
+++ system/Nix.nix
@@ -1,5 +1,10 @@
 { ... }: {
 	nixpkgs.config.allowUnfree = true;
+	nixpkgs.config.permittedInsecurePackages = [
+		"dotnet-runtime-6.0.36" # HACK: I hate Nix for this.
+		"dotnet-sdk-wrapped-6.0.428"
+		"dotnet-sdk-6.0.428"
+	];
 	nix.settings = {
 		auto-optimise-store = true;
 		keep-derivations = true;
