3aeaee716ef93086acb54967cbcec874fe64867b
Dmitry Voronin <hi@voronind.com>
2024-12-13 10:56:42 +0300

    Network: Replace fw rules with full network access.

--- host/x86_64-linux/pocket/Network.nix
+++ host/x86_64-linux/pocket/Network.nix
@@ -1,17 +1,9 @@
 { ... }: {
 	networking = {
 		firewall.extraCommands = ''
-			# Ssh access.
-			iptables  -I INPUT -j ACCEPT -s 10.0.0.0/8          -p tcp --dport 22143
-			ip6tables -I INPUT -j ACCEPT -s fd09:8d46:0b26::/48 -p tcp --dport 22143
-
-			# Syncthing.
-			iptables  -I INPUT -j ACCEPT -s 10.0.0.0/8 -p tcp --dport 22000
-			iptables  -I INPUT -j ACCEPT -s 10.0.0.0/8 -p udp --dport 22000
-			iptables  -I INPUT -j ACCEPT -s 10.0.0.0/8 -p udp --dport 21027
-			ip6tables -I INPUT -j ACCEPT -s fd09:8d46:0b26::/48 -p tcp --dport 22000
-			ip6tables -I INPUT -j ACCEPT -s fd09:8d46:0b26::/48 -p udp --dport 22000
-			ip6tables -I INPUT -j ACCEPT -s fd09:8d46:0b26::/48 -p udp --dport 21027
+			# Local access.
+			iptables  -I INPUT -j ACCEPT -s 10.0.0.0/8
+			ip6tables -I INPUT -j ACCEPT -s fd09:8d46:0b26::/48
 		'';
 	};
 }
