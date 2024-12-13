b39ab134e77ffd434f0494874c440b91b3ee361f
Dmitry Voronin <hi@voronind.com>
2024-12-14 00:32:55 +0300

    Sway: Toggle dnd on d key.

--- home/file/sway/module/System.nix
+++ home/file/sway/module/System.nix
@@ -6,10 +6,10 @@ in {
 		bindsym ${mod} input * xkb_switch_layout 0
 		bindsym --to-code ${mod}+a exec 'swayscript network'
 		bindsym --to-code ${mod}+c exec 'systemctl reboot -i'
+		bindsym --to-code ${mod}+d exec 'swayscript dnd'
 		bindsym --to-code ${mod}+g exec 'swayscript gaming'
 		bindsym --to-code ${mod}+l exec 'powerlimit toggle'
 		bindsym --to-code ${mod}+m exec 'swayscript monitor'
-		bindsym --to-code ${mod}+n exec 'swayscript dnd'
 		bindsym --to-code ${mod}+p exec 'powersave toggle'
 		bindsym --to-code ${mod}+r exec 'swayscript reload'
 		bindsym --to-code ${mod}+v exec 'swayscript vpn'
