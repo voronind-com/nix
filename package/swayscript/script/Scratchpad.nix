5ee8a8aeca72a94e109ef9ef4aefe34eadce67bb
Dmitry Voronin <hi@voronind.com>
2024-12-17 10:09:03 +0300

    Swayscript: Disable sound for scratchpad_kill.

--- package/swayscript/script/Scratchpad.nix
+++ package/swayscript/script/Scratchpad.nix
@@ -2,7 +2,7 @@
 	text = ''
 		# Kill all windows in Sway scratchpad.
 		function scratchpad_kill() {
-			notify_short
+			# notify_short
 			kill() {
 				for window in $(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?) | select(.name == "__i3_scratch").floating_nodes[].id'); do
 					swaymsg [ con_id="$window" ] kill
