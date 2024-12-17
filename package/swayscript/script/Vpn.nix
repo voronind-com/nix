ca3924a420816a1ce67989182269a9c9f5926737
Dmitry Voronin <hi@voronind.com>
2024-12-17 10:27:50 +0300

    Swayscript: Rename vpn to Vpn.

--- package/swayscript/script/Vpn.nix
+++ package/swayscript/script/Vpn.nix
@@ -4,14 +4,14 @@
 		function vpn() {
 			notify_short
 			if [[ "$(_vpn)" = "on" ]]; then
-				nmcli connection down vpn
+				nmcli connection down Vpn
 			else
-				nmcli connection up vpn
+				nmcli connection up Vpn
 			fi
 		}
 
 		function _vpn() {
-			local state=$(nmcli connection show vpn | rg -i state.*activated)
+			local state=$(nmcli connection show Vpn | rg -i state.*activated)
 			[ "''${state}" != "" ] && printf on || printf off
 		}
 	'';
