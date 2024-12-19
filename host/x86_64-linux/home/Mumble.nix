38ba41490b6928e4a8e4869a187ad456c574a83d
Dmitry Voronin <hi@voronind.com>
2024-12-19 22:40:00 +0300

    Mumble: Use pw file instead.

--- host/x86_64-linux/home/Mumble.nix
+++ host/x86_64-linux/home/Mumble.nix
@@ -1,13 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   services.murmur = {
     enable = true;
     clientCertRequired = true;
-    password = "bonjour";
+    environmentFile = "${config.services.murmur.stateDir}/Env";
     port = 22666;
     registerHostname = "chat.voronind.com";
     sslCert = "/etc/letsencrypt/live/voronind.com/fullchain.pem";
     sslKey = "/etc/letsencrypt/live/voronind.com/privkey.pem";
-    extraConfig = '''';
   };
 }
