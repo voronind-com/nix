1f5cfe7152a86d8889e3eea9d0157b85520a0537
Dmitry Voronin <hi@voronind.com>
2024-12-19 22:45:10 +0300

    Murmur: Fix password.

--- host/x86_64-linux/home/Mumble.nix
+++ host/x86_64-linux/home/Mumble.nix
@@ -4,6 +4,7 @@
     enable = true;
     clientCertRequired = true;
     environmentFile = "${config.services.murmur.stateDir}/Env";
+    password = "$MURMURD_PASSWORD";
     port = 22666;
     registerHostname = "chat.voronind.com";
     sslCert = "/etc/letsencrypt/live/voronind.com/fullchain.pem";
