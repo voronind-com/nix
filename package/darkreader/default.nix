d2e5cfb8f251e5a2aa416b1190775f4683f17151
Dmitry Voronin <hi@voronind.com>
2024-12-23 06:43:57 +0300

    Darkreader: Migrate to a new patch that persists non-policy settings.

--- package/darkreader/default.nix
+++ package/darkreader/default.nix
@@ -2,15 +2,18 @@
   __findFile,
   buildNpmPackage,
   fetchFromGitHub,
+  lib,
+  pkgs,
   ...
 }:
 buildNpmPackage rec {
-  version = "4.9.96";
+  version = "4.9.99";
   pname = "dark-reader";
-  npmDepsHash = "sha256-dSuCL8GZXiksqVQ+TypzOdAROn3q30ExaGCJu72GLyY=";
+  npmDepsHash = "sha256-m41HkwgbeRRmxJALQFJl/grYjjIqFOc47ltaesob1FA=";
+  env.ESBUILD_BINARY_PATH = lib.getExe pkgs.esbuild;
   patches = [ <patch/darkreader/Policy.patch> ];
   src = fetchFromGitHub {
-    hash = "sha256-2AYIFVTTMns1u0jKk3XeFuYdC1MfG9aOCMjAfZtlXuI=";
+    hash = "sha256-K375/4qOyE1Tp/T5V5uCGcNd1IVVbT1Pjdnq/8oRHj0=";
     owner = "darkreader";
     repo = "darkreader";
     rev = "v${version}";
