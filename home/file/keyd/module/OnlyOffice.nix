9f57c4194e10d939a4aaa3c9713efa75c6198be4
Dmitry Voronin <hi@voronind.com>
2024-12-25 06:22:16 +0300

    OnlyOffice: Add y/p to copy/paste.

--- home/file/keyd/module/OnlyOffice.nix
+++ home/file/keyd/module/OnlyOffice.nix
@@ -12,12 +12,14 @@
       "alt.j" = "down";
       "alt.k" = "up";
       "alt.l" = "right";
+      "alt.p" = "C-v";
       "alt.q" = "A-pageup";
       "alt.r" = "C-y";
       "alt.s" = "C-end";
       "alt.u" = "C-z";
       "alt.v" = "S-space";
       "alt.w" = "C-home";
+      "alt.y" = "C-c";
     };
   };
 }
