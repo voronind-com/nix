b9240f779dcffef566cb980b0b6817f421cc989e
Dmitry Voronin <hi@voronind.com>
2024-12-30 05:46:39 +0300

    Paperless: Fix nginx config.

--- host/x86_64-linux/home/nginx/Paperless.nix
+++ host/x86_64-linux/home/nginx/Paperless.nix
@@ -4,8 +4,8 @@
     listen 443 ssl;
 
     location / {
-    allow 10.0.0.0/8;
-    allow fd09:8d46:b26::/48;
+      allow 10.0.0.0/8;
+      allow fd09:8d46:b26::/48;
       deny all;
       proxy_pass http://127.0.0.1:28981$request_uri;
     }
