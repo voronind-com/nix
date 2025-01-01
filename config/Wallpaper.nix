3252822100f27c4a8c0060b9a590e50778ce0d19
Dmitry Voronin <hi@voronind.com>
2025-01-01 19:06:52 +0300

    Wallpaper: Fix non-video build.

--- config/Wallpaper.nix
+++ config/Wallpaper.nix
@@ -1,13 +1,20 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.module.wallpaper;
 in
 {
-  config = {
-    environment.systemPackages = [
-      (pkgs.writeShellScriptBin "wallpaper-video" ''
-        mpvpaper -o 'no-audio --loop-file --panscan=1' '*' ${cfg.videoPath}
-      '')
-    ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.videoPath != null) {
+      environment.systemPackages = [
+        (pkgs.writeShellScriptBin "wallpaper-video" ''
+          mpvpaper -o 'no-audio --loop-file --panscan=1' '*' ${cfg.videoPath}
+        '')
+      ];
+    })
+  ];
 }
