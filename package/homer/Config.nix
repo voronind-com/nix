cc23b6de16f7ff8a9bbf1d8789b9f83b7f5afd3c
Dmitry Voronin <hi@voronind.com>
2025-01-09 15:41:30 +0300

    Homer: Rename Calendar to Dav.

--- package/homer/Config.nix
+++ package/homer/Config.nix
@@ -4,9 +4,9 @@ let
   links = [ (mkLink "Status" "fa-heartbeat" "https://status.voronind.com") ];
   services = [
     (mkGroup "App" "fa-server" [
-      (mkLink "Calendar" "fa-calendar" "https://dav.voronind.com")
       (mkLink "Change" "fa-user-secret" "https://change.voronind.com")
       (mkLink "Craft" "fa-pen-ruler" "https://craft.voronind.com")
+      (mkLink "Dav" "fa-calendar" "https://dav.voronind.com")
       (mkLink "Download" "fa-download" "https://download.voronind.com")
       (mkLink "Git" "fab fa-git-alt" "https://git.voronind.com")
       (mkLink "Iot" "fa-home" "https://iot.voronind.com")
