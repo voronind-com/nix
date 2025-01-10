2909de81d4c958efa724af8cf53c58c0e379a86f
Dmitry Voronin <hi@voronind.com>
2025-01-10 09:47:09 +0300

    Thunderbird: Improve keyd mappings.

--- home/file/keyd/module/Thunderbird.nix
+++ home/file/keyd/module/Thunderbird.nix
@@ -8,16 +8,10 @@
       "alt.e" = "C-pagedown"; # Next tab.
       "alt.Q" = "C-S-pageup"; # Move tab left.
       "alt.E" = "C-S-pagedown"; # Move tab right.
-      "alt.a" = "A-left"; # Go back.
-      "alt.d" = "A-right"; # Go forward.
-      "alt.w" = "p"; # Paste and go.
       "alt.f" = "C-f"; # Find text.
       "alt.N" = "S-f3"; # Find prev.
       "alt.n" = "f3"; # Find next.
-      "alt.space" = "f6"; # Focus address bar.
-      "alt.r" = "C-f5"; # Full refresh.
-      "alt.m" = "C-m"; # Toggle tab mute.
-      "alt.s" = "A-p"; # Pin tab.
+      "alt.space" = "C-k"; # Focus search bar.
     };
   };
 }
