b102c4211e16dfbace8220b2fbb564b55dd957b6
Dmitry Voronin <hi@voronind.com>
2025-01-10 11:35:19 +0300

    Format.

--- option/Davmail.nix
+++ option/Davmail.nix
@@ -1,3 +1,6 @@
-{ config, lib, ... }: {
-  options.module.davmail.enable = lib.mkEnableOption "the Davmail Exchange proxy." // { default = config.module.purpose.work; };
+{ config, lib, ... }:
+{
+  options.module.davmail.enable = lib.mkEnableOption "the Davmail Exchange proxy." // {
+    default = config.module.purpose.work;
+  };
 }
