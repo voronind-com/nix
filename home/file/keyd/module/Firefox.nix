c30d0577361a5f8470f9793533f84dc22c65b572
Dmitry Voronin <hi@voronind.com>
2025-01-13 09:13:25 +0300

    Firefox: <A-w> to duplicate tab, <A-S-w> to move to a new window.

--- home/file/keyd/module/Firefox.nix
+++ home/file/keyd/module/Firefox.nix
@@ -2,25 +2,26 @@
 {
   file = (pkgs.formats.ini { }).generate "keyd-firefox-config" {
     "firefox-esr" = {
-      "alt.capslock" = "C-t"; # New tab.
-      "alt.x" = "C-w"; # Close tab.
-      "alt.u" = "C-S-t"; # Restore closed tab.
-      "alt.q" = "C-pageup"; # Prev tab.
-      "alt.e" = "C-pagedown"; # Next tab.
-      "alt.Q" = "C-S-pageup"; # Move tab left.
       "alt.E" = "C-S-pagedown"; # Move tab right.
-      "alt.a" = "A-left"; # Go back.
-      "alt.d" = "A-right"; # Go forward.
-      "alt.w" = "p"; # Paste and go.
-      "alt.f" = "C-f"; # Find text.
       "alt.N" = "S-f3"; # Find prev.
-      "alt.n" = "f3"; # Find next.
-      "alt.space" = "f6"; # Focus address bar.
-      "alt.r" = "C-f5"; # Full refresh.
+      "alt.Q" = "C-S-pageup"; # Move tab left.
+      "alt.a" = "A-left"; # Go back.
+      "alt.capslock" = "C-t"; # New tab.
+      "alt.d" = "A-right"; # Go forward.
+      "alt.e" = "C-pagedown"; # Next tab.
+      "alt.enter" = "C-S-l"; # Fill password.
+      "alt.f" = "C-f"; # Find text.
       "alt.l" = "A-S-a"; # Toggle dark mode.
       "alt.m" = "C-m"; # Toggle tab mute.
-      "alt.enter" = "C-S-l"; # Fill password.
+      "alt.n" = "f3"; # Find next.
+      "alt.q" = "C-pageup"; # Prev tab.
+      "alt.r" = "C-f5"; # Full refresh.
       "alt.s" = "A-p"; # Pin tab.
+      "alt.space" = "f6"; # Focus address bar.
+      "alt.u" = "C-S-t"; # Restore closed tab.
+      "alt.w" = "macro(y t)"; # Duplicate tab.
+      # "alt.W" = "macro(y t W)"; # Duplicate tab on new window. # ISSUE: Use S-w to manually detach.
+      "alt.x" = "C-w"; # Close tab.
     };
   };
 }
