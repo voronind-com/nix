57c3c8d17cc7b5bba9b347814aa62a514d8fdb0f
Dmitry Voronin <hi@voronind.com>
2025-01-26 11:35:07 +0300

    Wallpaper: Opposite room cat.

--- option/Wallpaper.nix
+++ option/Wallpaper.nix
@@ -9,8 +9,8 @@ let
   purpose = config.module.purpose;
 
   # Set the wallpaper here.
-  url = "https://share.voronind.com/wallpaper/TheUnbearableWeightOfMassiveTalent.mp4";
-  sha256 = "sha256-CT07Tb9Pegmi/KfteC+PPLcTXjXxbuicyDJr/XE17yY=";
+  url = "https://share.voronind.com/wallpaper/LiveWallpaperPcComOppositeRoomInTheRain1920X1080.mp4";
+  sha256 = "sha256-qamjLi9GCON0Xz6FtOuugUzac8ekXMJOdx2VDK1FA3g=";
   video = true;
 
   # Forse black and white for text.
