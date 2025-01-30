a4cd39faa21b6192687a2c7acadd60c9f6e6bbc4
Dmitry Voronin <hi@voronind.com>
2025-01-30 15:10:53 +0300

    Cups: Fix ipv6.

--- config/Print.nix
+++ config/Print.nix
@@ -9,7 +9,7 @@ in
       # NOTE: Cups server - Share/Allow remote printing on main page.
       clientConf = ''
         DigestOptions DenyMD5
-        ServerName 10.0.0.1
+        ServerName [fd09:8d46:b26:0:8079:82ff:fe1a:916a]
       '';
     };
   };
