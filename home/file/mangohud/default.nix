69e1a7f4bd35e8a59cab76fd0977cbbb17215a2f
Dmitry Voronin <hi@voronind.com>
2025-01-31 10:36:14 +0300

    MangoHud: Enable extra features.

--- home/file/mangohud/default.nix
+++ home/file/mangohud/default.nix
@@ -2,15 +2,18 @@
 {
   config = (pkgs.formats.iniWithGlobalSection { }).generate "MangoConfig" {
     globalSection = {
+      af = 16;
       blacklist = "example";
       fps_limit = "165,60,55,33,30,0";
       fps_limit_method = "early";
-      fps_sampling_period = 1000;
+      fps_sampling_period = 2000;
       frame_timing = 0;
+      gl_vsync = 0;
       preset = "0,1,2";
       toggle_fps_limit = "Shift_L+F2";
       toggle_logging = "Shift_L+F3";
       toggle_preset = "Shift_L+F1";
+      vsync = 0;
     };
   };
 
@@ -22,13 +25,13 @@
       alpha = 1.0;
       arch = 0;
       background_alpha = 0.5;
-      battery = 1;
+      battery = 0;
       battery_time = 1;
       benchmark_percentiles = 0;
       cpu_temp = 1;
-      device_battery = "gamepad,mouse";
+      # device_battery = "gamepad,mouse";
       font_size = 12;
-      fps_sampling_period = 1000;
+      fps_sampling_period = 2000;
       gpu_junction_temp = 0;
       gpu_mem_temp = 1;
       gpu_temp = 1;
