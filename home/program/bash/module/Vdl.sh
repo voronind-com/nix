073e59a5d1fb7fb9ad0e3b404bf9499873e63041
Dmitry Voronin <hi@voronind.com>
2025-02-03 23:41:21 +0300

    Vdl: Use eval for better arg processing.

--- home/program/bash/module/Vdl.sh
+++ home/program/bash/module/Vdl.sh
@@ -20,16 +20,10 @@ function vdl() {
 	fi
 
 	# Save [LINK] for later use.
-	[[ -f "Src.txt" ]] || echo "${target}" >Src.txt
+	[[ -f "Src.txt" ]] || echo "${target}" > Src.txt
 
 	# Download [LINK] content.
-	yt-dlp -4 -S 'res:1080,codec:av1,codec:vp9,codec:h264' --download-archive Index.txt --embed-thumbnail --embed-subs --write-auto-subs --embed-metadata --merge-output-format mkv -cio '%(playlist_index)000006d_%(id)s.%(ext)s' ${target} # || _vdl_retry
-}
-
-# Temporary fix for vk downloads.
-# Usage: vdl_vk <LINK>
-function vdl_vk() {
-	vdl --format mp4 "${@}"
+	eval "yt-dlp -4 -S 'res:1080,codec:av1,codec:vp9,codec:h264' --download-archive Index.txt --embed-thumbnail --embed-subs --write-auto-subs --embed-metadata --merge-output-format mkv -cio '%(playlist_index)000006d_%(id)s.%(ext)s'" "${target}"
 }
 
 # Download all videos from file with links.
