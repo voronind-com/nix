12be848e76bf9a3fe9a5139cddf2404de12ab05e
Dmitry Voronin <hi@voronind.com>
2025-02-07 20:37:26 +0300

    Tandoor: Fix user.

--- host/x86_64-linux/home/Tandoor.nix
+++ host/x86_64-linux/home/Tandoor.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ lib, ... }:
 {
+  systemd.services.tandoor-recipes.serviceConfig.DynamicUser = lib.mkForce false;
+  users = {
+    users.tandoor_recipes = {
+      group = "tandoor_recipes";
+      home = "/var/lib/tandoor_recipes";
+      isSystemUser = true;
+    };
+    groups.tandoor_recipes = {};
+  };
   services.tandoor-recipes = {
     enable = true;
     address = "[::1]";
