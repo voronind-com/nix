63e097dad57d9c0a90077ac2b19be0bd1d2530e9
Dmitry Voronin <hi@voronind.com>
2025-02-07 16:39:28 +0300

    Desktop: Add game pool to zfs boot.

--- host/x86_64-linux/desktop/Filesystem.nix
+++ host/x86_64-linux/desktop/Filesystem.nix
@@ -1,5 +1,7 @@
 { ... }:
 {
+  boot.zfs.extraPools = [ "game" ];
+
   # TODO: Move to <system>.
   fileSystems = {
     "/" = {
@@ -22,30 +24,5 @@
       device = "/dev/disk/by-partlabel/NIXBOOT";
       fsType = "vfat";
     };
-
-    # "/" = {
-    #   device = "/dev/disk/by-partlabel/NIXROOT";
-    #   fsType = "ext4";
-    #   options = [ "noatime" ];
-    # };
-    # "/storage/cold_1" = {
-    #   device = "/dev/storage/cold_1";
-    #   fsType = "ext4";
-    #   options = [
-    #     "noatime"
-    #     "noauto"
-    #     "nofail"
-    #   ];
-    # };
-    #
-    # "/storage/cold_2" = {
-    #   device = "/dev/storage/cold_2";
-    #   fsType = "ext4";
-    #   options = [
-    #     "noatime"
-    #     "noauto"
-    #     "nofail"
-    #   ];
-    # };
   };
 }
