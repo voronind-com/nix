07ed14b467f73375b202c4eb66f324169b45884b
Dmitry Voronin <hi@voronind.com>
2025-02-10 15:48:12 +0300

    Home: Sshd listen to vpn clients.

--- host/x86_64-linux/home/Ovpn.nix
+++ host/x86_64-linux/home/Ovpn.nix
@@ -29,31 +29,40 @@
   };
 
   # NOTE: Change the `server` to match `cfg.clients` or write a substring here.
-  services.openvpn.servers.vpn = {
-    autoStart = true;
-    config = ''
-      ca /var/lib/ovpn/pki/ca.crt
-      cert /var/lib/ovpn/pki/issued/home.crt
-      client-to-client
-      crl-verify /var/lib/ovpn/pki/crl.pem
-      dev tun
-      dh /var/lib/ovpn/dh2048.pem
-      explicit-exit-notify 1
-      group openvpn
-      ifconfig-pool-persist ipp.txt
-      keepalive 10 120
-      key /var/lib/ovpn/pki/private/home.key
-      persist-tun
-      port 22145
-      proto udp
-      push "dhcp-option DNS 10.0.0.1"
-      push "dhcp-option DNS 10.0.0.1"
-      push "route 10.0.0.0 255.0.0.0"
-      server 10.0.1.0 255.255.255.0
-      status openvpn-status.log
-      topology subnet
-      user openvpn
-      verb 4
-    '';
+  services = {
+    openssh.listenAddresses = [
+      {
+        addr = "10.0.1.1";
+        port = 22143;
+      }
+    ];
+
+    openvpn.servers.vpn = {
+      autoStart = true;
+      config = ''
+        ca /var/lib/ovpn/pki/ca.crt
+        cert /var/lib/ovpn/pki/issued/home.crt
+        client-to-client
+        crl-verify /var/lib/ovpn/pki/crl.pem
+        dev tun
+        dh /var/lib/ovpn/dh2048.pem
+        explicit-exit-notify 1
+        group openvpn
+        ifconfig-pool-persist ipp.txt
+        keepalive 10 120
+        key /var/lib/ovpn/pki/private/home.key
+        persist-tun
+        port 22145
+        proto udp
+        push "dhcp-option DNS 10.0.0.1"
+        push "dhcp-option DNS 10.0.0.1"
+        push "route 10.0.0.0 255.0.0.0"
+        server 10.0.1.0 255.255.255.0
+        status openvpn-status.log
+        topology subnet
+        user openvpn
+        verb 4
+      '';
+    };
   };
 }
