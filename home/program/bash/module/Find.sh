13b315b7c5ef26d3ff911e5370a3b6a4dfc94c99
Dmitry Voronin <hi@voronind.com>
2025-02-17 17:24:40 +0300

    Find: Show ext count.

--- home/program/bash/module/Find.sh
+++ home/program/bash/module/Find.sh
@@ -1,5 +1,16 @@
 # Find all file extensions.
 function find_ext() {
-	local types=($(find -type f | sed -e "s/.*\///" -e "s/^\.//" -e "/\./!d" -e "s/.*\.//"))
-	echo "${types[@]}" | tr ' ' '\n' | sort -u
+	local all=($(find -type f | sed -e "s/.*\///" -e "/\./!d" -e "s/.*\.//"))
+	declare -A types
+	for type in ${all[@]}; do
+		((types[${type}]++))
+	done
+
+	local result=()
+	for type in ${!types[@]}; do
+		result+=("${type} ${types[${type}]}")
+	done
+
+	local IFS=$'\n'
+	printf "%s\n" ${result[@]} | sort
 }
