7352463837fd09c81022e59b5b723ccce5f58aa7
Dmitry Voronin <hi@voronind.com>
2025-02-18 10:06:28 +0300

    Home: Disable scx server optimisations.

--- system/Scheduler.nix
+++ system/Scheduler.nix
@@ -1,10 +1,12 @@
 { config, ... }:
 let
-  purpose = config.module.purpose;
+  # purpose = config.module.purpose;
 
   scheduler = "scx_bpfland";
-  isServer = with purpose; server || router;
-  extraArgs = if isServer then [ "-c 0" ] else [ ]; # REF: https://github.com/sched-ext/scx/pull/1094
+  # ISSUE: Not working?
+  # isServer = with purpose; server || router;
+  # extraArgs = if isServer then [ "-c 0" ] else [ ]; # REF: https://github.com/sched-ext/scx/pull/1094
+  extraArgs = [ ];
 in
 {
   services.scx = {
