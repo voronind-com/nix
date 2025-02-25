ca88a059fa95941865b259d9a3013da937d7451f
Dmitry Voronin <hi@voronind.com>
2025-02-25 16:01:06 +0300

    Swayscript: wallpaper_video use notify_long to signal the stop.

--- package/swayscript/script/Wallpaper.sh
+++ package/swayscript/script/Wallpaper.sh
@@ -1,8 +1,10 @@
 # Toggle video wallpaper.
 function wallpaper_video() {
-	notify_short
 	re() {
-		pkill mpvpaper || wallpaper-video
+		pkill mpvpaper && notify_long || {
+			notify_short
+			wallpaper-video
+		}
 	}
 
 	_sway_iterate_sockets re
