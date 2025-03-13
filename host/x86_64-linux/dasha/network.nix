e333642c548648e20714be69244ba2ce6a109048
Dmitry Voronin <hi@voronind.com>
2025-03-13 15:47:28 +0300

    Ensure Wired connection for desktop systems.

--- host/x86_64-linux/dasha/network.nix
+++ host/x86_64-linux/dasha/network.nix
@@ -5,5 +5,21 @@
       # Local access.
       ip6tables -I INPUT -j ACCEPT -s ${secret.network.ula}
     '';
+
+    networkmanager.ensureProfiles.profiles = {
+      Wired = {
+        connection = {
+          id = "Wired";
+          type = "802-3-ethernet";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "eui64";
+          method = "auto";
+        };
+      };
+    };
   };
 }
