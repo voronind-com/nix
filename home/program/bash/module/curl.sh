81a38b196bc03612c7ca5354bc8e9298b9c58651
Dmitry Voronin <hi@voronind.com>
2025-03-17 10:49:15 +0300

    Bash: Rename dl to wdl.

--- home/program/bash/module/curl.sh
+++ home/program/bash/module/curl.sh
@@ -1,5 +1,5 @@
 # Download a file from the web.
-# Usaee: dl <FILE> [FILES...]
-function dl() {
+# Usaee: wdl <FILE> [FILES...]
+function wdl() {
 	wcurl --curl-options='--progress-bar --http2' -- "${@}"
 }
