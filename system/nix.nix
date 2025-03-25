d754a02a1fd9683e073e50b9cc58eeef29cc7b2b
Dmitry Voronin <hi@voronind.com>
2025-03-25 19:50:56 +0300

    nix: enable auto gc.

--- system/nix.nix
+++ system/nix.nix
@@ -1,19 +1,28 @@
 { inputs, ... }:
 {
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowInsecurePredicate = x: true; # HACK: Nix is fucking annoying.
-  nix.registry.nixpkgs.flake = inputs.nixpkgs;
-  nix.channel.enable = false;
-  nix.settings = {
-    auto-optimise-store = true;
-    keep-derivations = false;
-    keep-outputs = false;
-    min-free = 10 * 1000 * 1000 * 1000;
-    substituters = [ "https://nixos-cache-proxy.cofob.dev" ];
-    experimental-features = [
-      "flakes"
-      "nix-command"
-      "pipe-operators"
-    ];
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowInsecurePredicate = x: true; # HACK: Nix is fucking annoying.
+  };
+  nix = {
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    channel.enable = false;
+    settings = {
+      auto-optimise-store = true;
+      keep-derivations = false;
+      keep-outputs = false;
+      min-free = 10 * 1000 * 1000 * 1000;
+      substituters = [ "https://nixos-cache-proxy.cofob.dev" ];
+      experimental-features = [
+        "flakes"
+        "nix-command"
+        "pipe-operators"
+      ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 }
